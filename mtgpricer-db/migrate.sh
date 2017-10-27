20dca77bf0a70376d7bf5cab25f2f14df3869ef9
Jared Pearson <jaredapearson@gmail.com>
2017-10-27 16:56:27 -0700

    Added check for flyway in migrate script

--- mtgpricer-db/migrate.sh
+++ mtgpricer-db/migrate.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# check if required deps are installed
+if ! [ -x "$(command -v flyway)" ]; then
+    echo "Flyway is not installed"
+    exit 1;
+fi
+
 flyway -configFile="$PWD/flyway.properties" -password='d!rtyDanc1ng' -locations="filesystem:$PWD/sql" migrate
