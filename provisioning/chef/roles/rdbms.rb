562c8614e6c1cdaf79988b13a5becc6323fa7bd3
Jared Pearson <jaredapearson@gmail.com>
2017-10-27 16:55:52 -0700

    Updated to postgres 9.5

--- provisioning/chef/roles/rdbms.rb
+++ provisioning/chef/roles/rdbms.rb
@@ -1,12 +1,27 @@
 name "rdbms"
 
-default_attributes(
+override_attributes(
 	"postgresql" => {
+		"version" => "9.5",
+		"dir" => "/etc/postgresql/9.5/main",
 		"password" => {
 			"postgres" => "d!rtyDanc1ng"
 		},
+		"client" => {
+			"packages" => [
+				"postgresql-client-9.5",
+				"libpq-dev"
+			]
+		},
+		"server" => {
+			"service_name" => "postgresql",
+			"packages" => [
+				"postgresql-9.5"
+			]
+		},
 		"contrib" => {
 			"packages" => [
+				"postgresql-contrib-9.5",
 				"pgcrypto"
 			],
 			"extensions" => [
@@ -14,7 +29,8 @@ default_attributes(
 			]
 		},
 		"config" => {
-			"listen_addresses" => "*"
+			"listen_addresses" => "*",
+			"port" => 5432
 		},
 		"pg_hba" => [
 			{
@@ -22,6 +38,13 @@ default_attributes(
 				:db => 'all', 
 				:user => 'postgres,mtgpricer_app', 
 				:addr => '192.168.37.0/24', 
+				:method => 'password'
+			},
+			{
+				:type => 'host', 
+				:db => 'all', 
+				:user => 'all', 
+				:addr => '0.0.0.0/0', 
 				:method => 'password'
 			}
 		]
